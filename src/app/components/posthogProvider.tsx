8c8eee75041f69c808e03ae371866b1b6121c169
Harry Thomas <hsthomas@verisk.com>
2026-04-19 21:51:13 -0600

    fix: don't throw when NEXT_PUBLIC_POSTHOG_KEY is absent in CI

--- src/app/components/posthogProvider.tsx
+++ src/app/components/posthogProvider.tsx
@@ -5,13 +5,12 @@ import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
 import { sampleByEvent } from 'posthog-js/lib/src/customizations/before-send.js'
 
-if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-  throw new Error('NEXT_PUBLIC_POSTHOG_KEY is not set')
-}
 const runningInProduction =
-  process.env.NODE_ENV === 'production' && typeof window !== 'undefined'
+  process.env.NODE_ENV === 'production' &&
+  typeof window !== 'undefined' &&
+  !!process.env.NEXT_PUBLIC_POSTHOG_KEY
 if (runningInProduction) {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: '/ingest',
     ui_host: 'https://us.posthog.com',
     before_send: sampleByEvent(['$web_vitals'], 0.5),
